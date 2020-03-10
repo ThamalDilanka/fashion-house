9e1f5d4981a9a48de57ef06740b4d1a1894d67da
Thamal Dilanka <thamaldilanke@gmail.com>
2020-03-10 11:32:09 +0530

    Added config folder to initialize the DB connection and connected to the MongoDB remote server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('API Running'));
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const express = require('express')
+const connectDB = require('./config/db')
 
+const app = express()
+
+// Connecting to the database
+connectDB()
+
+app.get('/', (req, res) => res.send('API Running'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
