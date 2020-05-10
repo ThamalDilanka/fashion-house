cac3dc87da353367ec0b674c0fd2de0d0e591a7e
Chamod Rathnayake <chamod@getshoutout.com>
2020-05-10 16:35:55 +0530

    changes to the initial commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const productController = require('./../controllers/productController');
 const multer = require('multer');
-const  upload = multer({dest: 'public/img/items'});
+const dest = process.env.DOWNLOAD_DESTINATION
+const  upload = multer({dest: `${__dirname}`+dest});
 
 const router = express.Router();
 
