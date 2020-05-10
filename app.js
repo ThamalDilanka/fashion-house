21433ac8be219dde5740acebb11375d8029859be
Thamal Dilanka <thamaldilanke@gmail.com>
2020-05-11 00:08:09 +0530

    Body parser added

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 // Importing routes from the routes folder
 const userRoutes = require('./routes/userRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const favouriteRoutes = require('./routes/favouriteRoutes');
 
 // Creating a Express application
 const app = express();
+
+// Setting body parser to get access of request.body
+app.use(bodyParser.json());
 
 // Logging all the requests to the console in development environment
 if (process.env.NODE_ENV === 'development') {
@@ -17,6 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Setting the routes to the app as middlewares
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/categories', categoryRoutes);
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/carts', cartRoutes);
 app.use('/api/v1/favourites', favouriteRoutes);
