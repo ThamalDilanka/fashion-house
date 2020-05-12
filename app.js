b3ebc8050c4eea43bc2a3686de2a12b3d2641b8b
Thamal Dilanka <thamaldilanke@gmail.com>
2020-05-12 07:10:06 +0530

    added feature to handle unknown routes

--- app.js
+++ app.js
@@ -31,4 +31,12 @@ app.use('/api/v1/favourites', favouriteRoutes);
 app.use('/api/v1/orders', orderRoutes);
 app.use('/api/v1/reviews', reviewRoutes);
 
+// Handle unknown routes
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Can't find ${req.originalUrl} on this server!`
+	})
+})
+
 module.exports = app;
