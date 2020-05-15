95bda9b86d933cd44b3e822d0bf965009c97d448
Chamod Rathnayake <chamod@getshoutout.com>
2020-05-16 00:14:34 +0530

    modal added

--- client/src/components/DiscountCards/DiscountCard/DiscountCard.js
+++ client/src/components/DiscountCards/DiscountCard/DiscountCard.js
@@ -6,6 +6,28 @@ const DiscountCard = (data) => {
     return (
         <div className="container">
 
+            {/* Modal */}
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">model discount</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            Test
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Modal ends*/}
+
             <div className="mt-5">
                 <table className="table table-striped">
                     <thead className="thead-dark">
@@ -20,15 +42,15 @@ const DiscountCard = (data) => {
                     </thead>
                     <tbody>
                         {
-                             data.data && data.data.map(data =>
+                            data.data && data.data.map(data =>
                                 <tr>
                                     {/* <th scope="row">1</th> */}
-                                    <td><img src={`./../../../../public/images/products/${data.images[0]}`} alt="category"/></td>
+                                    <td><img src={`./../../../../public/images/products/${data.images[0]}`} alt="category" /></td>
                                     <td>{data.name}</td>
                                     <td>{data.price}</td>
                                     <td>{data.description}</td>
-                                    <td>{data.discount.percentage+'%'}</td>
-                                    <td>{<button type="button" class="btn btn-outline-secondary">Add Discount</button>}</td>
+                                    <td>{data.discount.percentage + '%'}</td>
+                                    <td>{<button type="button" data-toggle="modal" data-target="#exampleModal" class="btn btn-outline-secondary">Add Discount</button>}</td>
                                 </tr>
                             )}
 
