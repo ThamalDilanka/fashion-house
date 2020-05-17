c32ccc803f9c74de7f228e76768a196ea7642ac6
Chamod Rathnayake <chamod@getshoutout.com>
2020-05-17 21:51:18 +0530

    modal initialized for take value

--- client/src/containers/Categories.js
+++ client/src/containers/Categories.js
@@ -1,19 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect} from 'react'
 import axios from 'axios';
 
 const Categories = () => {
 
     const [getCategories, setGetCategories] = useState([]);
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
 
     useEffect(() => { //retrieve categories on pg load
         const fetchData = async () => {
             const resp = await axios.get('http://localhost:8000/api/v1/categories');
             setGetCategories([...resp.data.data.categories]);
-            // console.log(resp.data.data.categories);
         }
         fetchData();
     }, []);
     { console.log("test: ", getCategories) }
+
+    const addNewCategory = (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token') //temp - change  token
+        axios({
+            method: 'patch',
+            url: `http://localhost:8000/api/v1/products`,
+            data: {
+                title: title ,
+                description: description,
+                image:''
+            },
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+        });
+        window.location.reload()
+    }
 
     return (
 
@@ -31,11 +50,31 @@ const Categories = () => {
                                 </button>
                             </div>
                             <div className="modal-body">
-                                ...
+                                <form>
+                                    <div className="form-group">
+                                        <label>Add Title</label>
+                                        <input type="text" className="form-control" id="exampleInputTitle"
+                                        autoComplete="off"
+                                        value={title}
+                                        onChange={e => setTitle(e.target.value)}/>
+                                        <small id="emailHelp" className="form-text text-muted">Give a name to new Category</small>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Description</label>
+                                        <input type="text" className="form-control" id="exampleInputDescription" 
+                                        autoComplete="off"
+                                        value={description}
+                                        onChange={e => setDescription(e.target.value)}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Add image</label>
+                                        <input type="file" className="form-control" id="exampleInputPic" />
+                                    </div>
+                                </form>
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" className="btn btn-primary">Understood</button>
+                                <button type="button" className="btn btn-primary" onClick={(e) => {addNewCategory(e)}}>Add</button>
                             </div>
                         </div>
                     </div>
