b199cf12645900e291aa139b07af5877bfc4ed7e
Chamod Rathnayake <chamod@getshoutout.com>
2020-05-17 20:20:59 +0530

    setup a view for category add

--- client/src/containers/Categories.js
+++ client/src/containers/Categories.js
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Categories = () => {
     return (
-        <div>
-            <p>add categories component initialized</p>
+        <div className="mt-5">
+            <div className="container">
+                <table className="table  table-striped">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope="col">Add Category</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
