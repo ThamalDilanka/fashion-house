d6104e1ede13863755e23969218f26784b3c7c09
Thamal Dilanka <thamaldilanke@gmail.com>
2020-05-24 15:02:51 +0530

    show case protected

--- client/src/containers/ShowCase.js
+++ client/src/containers/ShowCase.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProductCards from '../components/ProductCards/ProductCards';
+import { Redirect } from 'react-router';
 
 const ShowCase = (props) => {
-  const [products, setProducts] = useState([]);
-  const categoryIdHash = props.location.hash;
-  const categoryId = categoryIdHash.substring(1);
+	const [products, setProducts] = useState([]);
+	const categoryIdHash = props.location.hash;
+	const categoryId = categoryIdHash.substring(1);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8000/api/v1/products?category=${categoryId}`)
-      .then((res) => setProducts(res.data.data.products))
-      .catch((err) => console.log(err));
-  }, []);
+	useEffect(() => {
+		if (categoryId === '') {
+			return;
+		}
 
-  return (
-    <div className="my-5 container">
-      <ProductCards products={products} />
-    </div>
-  );
+		axios
+			.get(`http://localhost:8000/api/v1/products?category=${categoryId}`)
+			.then((res) => setProducts(res.data.data.products))
+			.catch((err) => console.log(err));
+	}, []);
+
+	return (
+		<React.Fragment>
+      {categoryId === '' ? <Redirect to='/not-found' /> : null}
+			<div className='my-5 container'>
+				<ProductCards products={products} />
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default ShowCase;
